b002fdc532151b24b8ab58b3050766dffa5aa248
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-07-16 10:51:27 -0300

    feat : rest client vscode extension

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
-import { CreateAcountController } from './controllers/create-account.controller'
+import { CreateAccountController } from './controllers/create-account.controller'
 
 @Module({
   imports: [],
-  controllers: [CreateAcountController],
+  controllers: [CreateAccountController],
   providers: [PrismaService],
 })
 export class AppModule {}
