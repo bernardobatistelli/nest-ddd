0eb7920e683e7b8efe1ef2633425d11daa347226
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-07-17 11:36:53 -0300

    feat : copying domain layer from ddd project

---
+++ src/core/entities/aggregate-root.ts
@@ -0,0 +1,21 @@
+import { DomainEvent } from '../events/domain-event'
+import { DomainEvents } from '../events/domain-events'
+import { Entity } from './entity'
+
+export abstract class AggregateRoot<Props> extends Entity<Props> {
+  private _domainEvents: DomainEvent[] = [] // forma do agregado anotar os eventos que disparou
+
+  get domainEvents(): DomainEvent[] {
+    return this._domainEvents
+  }
+
+  // pré disparar os eventos: jogar na classe, antes de ser validado no banco de dados
+  protected addDomainEvent(domainEvent: DomainEvent): void {
+    this._domainEvents.push(domainEvent)
+    DomainEvents.markAggregateForDispatch(this)
+  }
+
+  public clearEvents() {
+    this._domainEvents = []
+  }
+}
