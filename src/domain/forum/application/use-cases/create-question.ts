66d94f633287df26ce0936e80fba0ebfa689d73f
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-09-10 11:21:46 -0300

    refactor : changing controllers to private as default

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -3,7 +3,7 @@ import { QuestionsRepository } from '../repositories/questions-repository'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Either, right } from '@/core/either'
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
-import { QuestionAttachmentList } from '../../enterprise/entities/question-attachment-list'
+import { QuestionAttachmentList } from '@/domain/forum/enterprise/entities/question-attachment-list'
 import { Injectable } from '@nestjs/common'
 
 interface CreateQuestionUseCaseRequest {
